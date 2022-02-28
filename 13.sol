ddec540d97337beb6c7ef85ec488b0557f99f62c
Fernando Lopez <37802572+Cainuriel@users.noreply.github.com>
2022-02-28 15:37:31 +0100

    Create 13.sol

---
+++ 13.sol
@@ -0,0 +1,6 @@
+{
+"description": "NFT ORO de la banda Queen",
+"external_url": "https://topstars.app/es/inicioes/",
+"image": "https://raw.githubusercontent.com/Cainuriel/metadatas/main/poap.png",
+"name": "QUEEN NFT ORO"
+}
